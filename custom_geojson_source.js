4661e6cd26d368921793e89ebbb156ffc06d954f
Oscar Fonts <oscar.fonts@gmail.com>
2017-05-30 12:11:55 +0200

    ES6 syntax

--- custom_geojson_source.js
+++ custom_geojson_source.js
@@ -1,7 +1,11 @@
-var GeoJSONSource = require('mapbox-gl/src/source/geojson_source');
-var webworkify = require('webworkify');
+const GeoJSONSource = require('mapbox-gl/src/source/geojson_source');
+const webworkify = require('webworkify');
 
 class CustomGeoJSONSource extends GeoJSONSource {
+    constructor(id, options, dispatcher, eventedParent) {
+        super(id, options, dispatcher, eventedParent);
+        this.type="custom_geojson";
+    }
 }
 
 CustomGeoJSONSource.workerSourceURL = URL.createObjectURL(webworkify(require('./custom_geojson_worker.js'), {bare: true}));
