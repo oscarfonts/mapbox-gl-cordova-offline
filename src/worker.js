d67efa6f7fefa07fc1225f0b967756f3edd8c335
David Martin <david@itbeyond.com.au>
2020-06-18 12:52:13 +0800

    Update worker.js for mapbox-gl-js 1.11.0
    I found a number of updates needed in the worker.js file to bring repo file to latest mapbox.

--- src/worker.js
+++ src/worker.js
@@ -9,6 +9,7 @@ import RasterTileSourceOffline from './raster_tile_offline_source';
 import GeoJSONWorkerSource from 'mapbox-gl/src/source/geojson_worker_source';
 import assert from 'assert';
 import { plugin as globalRTLTextPlugin } from 'mapbox-gl/src/source/rtl_text_plugin';
+import {enforceCacheSizeLimit} from 'mapbox-gl/src/util/tile_request_cache';
 
 import type {
     WorkerSource,
@@ -30,6 +31,7 @@ export default class Worker {
     self: WorkerGlobalScopeInterface;
     actor: Actor;
     layerIndexes: { [string]: StyleLayerIndex };
+    availableImages: {[_: string]: Array<string> };
     workerSourceTypes: { [string]: Class<WorkerSource> };
     workerSources: { [string]: { [string]: { [string]: WorkerSource } } };
     demWorkerSources: { [string]: { [string]: RasterDEMTileWorkerSource } };
@@ -40,6 +42,7 @@ export default class Worker {
         this.actor = new Actor(self, this);
 
         this.layerIndexes = {};
+        this.availableImages = {};
 
         this.workerSourceTypes = {
             vector: VectorTileWorkerSource,
@@ -59,8 +62,9 @@ export default class Worker {
             this.workerSourceTypes[name] = WorkerSource;
         };
 
+        // This is invoked by the RTL text plugin when the download via the `importScripts` call has finished, and the code has been parsed.
         this.self.registerRTLTextPlugin = (rtlTextPlugin: {applyArabicShaping: Function, processBidirectionalText: Function, processStyledBidirectionalText?: Function}) => {
-            if (globalRTLTextPlugin.isLoaded()) {
+            if (globalRTLTextPlugin.isParsed()) {
                 throw new Error('RTL text plugin already registered.');
             }
             globalRTLTextPlugin['applyArabicShaping'] = rtlTextPlugin.applyArabicShaping;
@@ -71,6 +75,17 @@ export default class Worker {
 
     setReferrer(mapID: string, referrer: string) {
         this.referrer = referrer;
+    }
+
+    setImages(mapId: string, images: Array<string>, callback: WorkerTileCallback) {
+        this.availableImages[mapId] = images;
+        for (const workerSource in this.workerSources[mapId]) {
+            const ws = this.workerSources[mapId][workerSource];
+            for (const source in ws) {
+                ws[source].availableImages = images;
+            }
+        }
+        callback();
     }
 
     setLayers(mapId: string, layers: Array<LayerSpecification>, callback: WorkerTileCallback) {
@@ -146,17 +161,33 @@ export default class Worker {
         }
     }
 
-    loadRTLTextPlugin(map: string, pluginURL: string, callback: Callback<void>) {
+    syncRTLPluginState(map: string, state: PluginState, callback: Callback<boolean>) {
         try {
-            if (!globalRTLTextPlugin.isLoaded()) {
+            globalRTLTextPlugin.setState(state);
+            const pluginURL = globalRTLTextPlugin.getPluginURL();
+            if (
+                globalRTLTextPlugin.isLoaded() &&
+                !globalRTLTextPlugin.isParsed() &&
+                pluginURL != null // Not possible when `isLoaded` is true, but keeps flow happy
+            ) {
                 this.self.importScripts(pluginURL);
-                callback(globalRTLTextPlugin.isLoaded() ?
-                    null :
-                    new Error(`RTL Text Plugin failed to import scripts from ${pluginURL}`));
+                const complete = globalRTLTextPlugin.isParsed();
+                const error = complete ? undefined : new Error(`RTL Text Plugin failed to import scripts from ${pluginURL}`);
+                callback(error, complete);
             }
         } catch (e) {
             callback(e.toString());
         }
+    }
+
+    getAvailableImages(mapId: string) {
+        let availableImages = this.availableImages[mapId];
+
+        if (!availableImages) {
+            availableImages = [];
+        }
+
+        return availableImages;
     }
 
     getLayerIndex(mapId: string) {
@@ -181,8 +212,7 @@ export default class Worker {
                     this.actor.send(type, data, callback, mapId);
                 }
             };
-
-            this.workerSources[mapId][type][source] = new (this.workerSourceTypes[type]: any)((actor: any), this.getLayerIndex(mapId));
+            this.workerSources[mapId][type][source] = new (this.workerSourceTypes[type]: any)((actor: any), this.getLayerIndex(mapId), this.getAvailableImages(mapId));
         }
 
         return this.workerSources[mapId][type][source];
@@ -197,6 +227,10 @@ export default class Worker {
         }
 
         return this.demWorkerSources[mapId][source];
+    }
+
+    enforceCacheSizeLimit(mapId: string, limit: number) {
+        enforceCacheSizeLimit(limit);
     }
 }
 
